e646f868ddad2dac201f6a41be941c70406991cf
SchoolPoole <143743475+SchoolPoole@users.noreply.github.com>
2023-08-31 13:38:04 -0400

    Add files via upload

---
+++ main5017.py
@@ -0,0 +1,12 @@
+#main function
+
+def main():
+    value = int(17)
+    changeValue()
+    print('The value is', int(value))
+
+def changeValue():
+    value = int(50)
+    print('The value is', int(value))
+#call main
+main()
